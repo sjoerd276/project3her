48bbfcfada3982b91ab1289e947967c037b96d88
Sjoerd de Graaff <sjoerddegraaff@live.nl>
2019-02-19 11:50:24 +0100

    Blank app

--- project3her/Program.cs
+++ project3her/Program.cs
@@ -17,8 +17,6 @@ namespace project3her
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            // test123
         }
     }
 }
