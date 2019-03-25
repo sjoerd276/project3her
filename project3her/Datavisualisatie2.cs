7f198004bf9705707bd7ea82ba525386e902b29d
Jimmy <jimmy.balgobiend@gmail.com>
2019-03-25 11:42:27 +0100

    GetData

--- project3her/Datavisualisatie2.cs
+++ project3her/Datavisualisatie2.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
 
 namespace project3her
 {
@@ -22,6 +25,44 @@ namespace project3her
             Form1 menu = new Form1();
             menu.Show();
             this.Close();
+        }
+
+        private void Datavisualisatie2_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT(SELECT COUNT(Voorval_nr)FROM straatroof_2011) AS count1,(SELECT COUNT(Voorval_nummer)FROM bikebike_theft) AS count2 FROM dual";
+
+
+            
+
+            DataTable dt = GetData(query);
+
+            chart1.DataSource = dt;
+
+
+            chart1.Series["Straatroof"].YValueMembers = "count1";
+            chart1.Series["Fietsdiefstal"].YValueMembers = "count2";
+
+            chart1.Titles.Add("Diefstal");
+
+        }
+
+        private static DataTable GetData(string query)
+        {
+            string server = "localhost";
+            string database = "project3";
+            string uid = "project3";
+            string password = "project3";
+            string constr = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                using (MySqlDataAdapter sda = new MySqlDataAdapter(query, con))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    return dt;
+                }
+            }
         }
     }
 }
