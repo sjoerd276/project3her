5998d64f5c36535c2cb971e9a9eb1bcab724b869
Jimmy <jimmy.balgobiend@gmail.com>
2019-04-18 13:35:12 +0200

    Filter op dag. Datavisualisatie2

--- project3her/Datavisualisatie2.Designer.cs
+++ project3her/Datavisualisatie2.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBoxJaar = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(18, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Menu";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,31 +52,62 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(254, 72);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(381, 111);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Straatroof";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Fietsdiefstal";
-            this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Straatroof";
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "Fietsdiefstal";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(450, 462);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
             // 
+            // comboBoxJaar
+            // 
+            this.comboBoxJaar.FormattingEnabled = true;
+            this.comboBoxJaar.Items.AddRange(new object[] {
+            "MA",
+            "DI",
+            "WO",
+            "DO",
+            "VR",
+            "ZA",
+            "ZO"});
+            this.comboBoxJaar.Location = new System.Drawing.Point(1000, 149);
+            this.comboBoxJaar.Name = "comboBoxJaar";
+            this.comboBoxJaar.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxJaar.TabIndex = 2;
+            this.comboBoxJaar.SelectedIndexChanged += new System.EventHandler(this.comboBoxJaar_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1110, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Datavisualisatie2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBoxJaar);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Datavisualisatie2";
             this.Text = "Datavisualisatie2";
             this.Load += new System.EventHandler(this.Datavisualisatie2_Load);
@@ -86,5 +120,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBoxJaar;
+        private System.Windows.Forms.Button button2;
     }
 }
--- project3her/Datavisualisatie2.cs
+++ project3her/Datavisualisatie2.cs
@@ -64,5 +64,36 @@ namespace project3her
                 }
             }
         }
+
+        private void comboBoxJaar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxJaar != null)
+            {
+                chart1.Series["Straatroof"].Points.Clear();
+                chart1.Series["Fietsdiefstal"].Points.Clear();
+
+
+                string Filter = ((string)comboBoxJaar.SelectedItem);
+                
+
+                string query = "SELECT(SELECT COUNT(Voorval_nr)FROM straatroof_2011 WHERE dagsoort ='" + comboBoxJaar.Text.ToString() + "') AS count1,(SELECT COUNT(Voorval_nummer)FROM bikebike_theft WHERE Begin_dagsoort ='" + comboBoxJaar.Text.ToString() + "') AS count2 FROM dual";
+
+
+                DataTable dt = GetData(query);
+
+                chart1.DataSource = dt;
+
+
+                chart1.Series["Straatroof"].YValueMembers = "count1";
+                chart1.Series["Fietsdiefstal"].YValueMembers = "count2";
+
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
